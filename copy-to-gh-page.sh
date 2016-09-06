147d3f779a6c0ff61595a2112b212c145dc5a3d7
Sylvain Jermini <sylvain.jermini@syjer.com>
2016-09-06 11:04:16 +0200

    script

--- copy-to-gh-page.sh
+++ copy-to-gh-page.sh
@@ -6,3 +6,17 @@ rm -rf tmp-lavagna-site
 mv target/site tmp-lavagna-site
 git stash
 git checkout gh-pages
+rm -f CNAME
+rm -rf download  
+rm -rf images
+rm -rf js
+rm start.txt
+rm -rf css
+rm -rf help
+rm -f index.html  
+rm -rf roadmap  
+rm stop.txt
+mv tmp-lavagna-site/* .
+rmdir tmp-lavagna-site
+git add -A
+
